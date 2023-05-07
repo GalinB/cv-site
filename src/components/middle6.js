a1629d866cda147da46afb0efdc99f4bdb6c77e5
GalinB <galin.bozhkov@gmail.com>
2023-05-07 18:01:41 +0300

    another change

--- src/components/middle6.js
+++ src/components/middle6.js
@@ -4,7 +4,7 @@ import { FaRegCopyright } from 'react-icons/fa'
 export default function Middle6({ darkMode }) {
   return (
     <div className={darkMode ? 'middle6' : 'darkMiddle6'}>
-      <h1 className="middle6h1">
+      <h1 className={darkMode ? 'middle6h1' : 'darkMiddle6h1'}>
         {' '}
         <FaRegCopyright /> 2024 All Rights Reserved.Bozhkov
       </h1>
